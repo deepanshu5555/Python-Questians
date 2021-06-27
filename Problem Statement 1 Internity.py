6e3c8ad1bfd03ca0df597972f257e590a6478761
DEEPANSHU CHAUHAN <70942316+deepanshu5555@users.noreply.github.com>
2021-06-27 11:28:26 +0530

    Add files via upload

---
+++ Problem Statement 1 Internity.py
@@ -0,0 +1,31 @@
+import random
+print("Welcome to the number guessing game! Enter any number between 330 to 430")
+x=random.randint(330,430)
+n=8
+print("You only have 8 attamps")
+flag=False
+while(n):
+    print("Enter Your number")
+    a=int(input())
+    if(a==x):
+        print("Yeah! You identified the number")
+        flag=True
+        break
+    elif(a>x):
+        if(n!=1):
+            print("Please try again! The number you guessed is too high")
+    else:
+        if(n!=1):
+            print("Please try again! The number you guessed is too small")
+    n=n-1
+if(flag is False):
+    print("Oops! All you chances are finished. Better luck next time!")
+print("The number was ",x)
+    
+    
+                
+                
+            
+    
+    
+        
